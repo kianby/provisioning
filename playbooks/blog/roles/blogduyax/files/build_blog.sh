ae5bad4e92327b2ea5fd553eded0abc4c15b16a8
Yax <kianby@madyanne.fr>
2015-06-03 13:26:04 +0200

    Add build to CRON, Generate certificate

--- playbooks/blog/roles/blogduyax/files/build_blog.sh
+++ playbooks/blog/roles/blogduyax/files/build_blog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+forced=${1:?"no"};shift
+
 # add virtualenv to path
 export PATH=$PATH:/root/.virtualenvs/pelican/bin
 
@@ -7,27 +9,28 @@ mkdir -p /var/www/blogduyax >/dev/null
 
 cd /usr/local/src/blogduyax
 
-echo "Pelican is checking if publishing is needed..."
-
-# git pull
-git fetch origin
-reslog=$(git log HEAD..origin/master --oneline)
-if [[ "${reslog}" = "" ]] ; then
-    echo "Repository has not changed. Nothing to do"
-else
-    echo "Repository has changed. Merge master branch"
-    git merge origin/master
-
-    # build
-    echo "Start Pelican build process"
-    make publish
-    RETVAL=$?
-    if [ $RETVAL -ne 0 ]; then
-        echo "Build failed. It is not safe to deploy"
+if [[ "$forced" = "no" ]] ; then
+    echo "Pelican is checking if publishing is needed..."
+    git fetch origin
+    reslog=$(git log HEAD..origin/master --oneline)
+    if [[ "${reslog}" = "" ]] ; then
+        echo "Repository has not changed. Nothing to do"
+        exit 0
     else
-        echo "Build successful. Deploying..."
-        rm -rf /var/www/blogduyax/*
-        cp -r /usr/local/src/blogduyax/output/* /var/www/blogduyax/.
-        chown -R www-data:www-data /var/www/blogduyax
+        echo "Repository has changed. Merge master branch"
+        git merge origin/master
     fi
+fi
+
+# build
+echo "Start Pelican build process"
+make publish
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    echo "Build failed. It is not safe to deploy"
+else
+    echo "Build successful. Deploying..."
+    rm -rf /var/www/blogduyax/*
+    cp -r /usr/local/src/blogduyax/output/* /var/www/blogduyax/.
+    chown -R www-data:www-data /var/www/blogduyax
 fi
